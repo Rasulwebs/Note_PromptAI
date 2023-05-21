98613e9abbc4b1a85f3b25d22b4502026b7a828a
Rasulwebs <boboqulovrasul80@gmail.com>
2023-05-21 23:26:40 +0500

    editing routes contunue

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -7,7 +7,6 @@ export const POST = async (request) => {
     try {
         await connectToDB();
         const newPrompt = new Prompt({ creator: userId, prompt, tag });
-
         await newPrompt.save();
         return new Response(JSON.stringify(newPrompt), { status: 201 })
     } catch (error) {
