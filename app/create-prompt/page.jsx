48f4d6168cbb6497cab7ac20afc209a8797c39dd
Rasulwebs <boboqulovrasul80@gmail.com>
2023-05-28 23:26:51 +0500

    editing pages

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,7 @@
 "use client";
-
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
